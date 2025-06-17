2abd593dc14d58a24d5dabe2c8baf5095f4f4518
Wojtek Bauman <wojciech.bauman@cloudfide.com>
2025-06-17 12:22:42 +0200

    chore: fix scripts

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,9 @@
+create sequence public.employee_id_seq
+    increment by 50;
+
 create table public.employees
 (
-    id    integer primary key not null default nextval('employees_id_seq'::regclass),
+    id integer primary key not null default nextval('employee_id_seq'::regclass),
     name  text                not null,
     pesel bytea               not null
 );
-
-create sequence public.employee_id_seq
-    increment by 50;
